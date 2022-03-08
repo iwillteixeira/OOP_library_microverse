aa09021f258295ce4cce6b737fc6261487796c6f
iwillteixeira <iwill2005@gmail.com>
2022-03-07 21:17:06 -0300

    Fix suggested changes

--- person.rb
+++ person.rb
@@ -1,9 +1,10 @@
 #!/usr/bin/env ruby
 class Person
-  def initialize(age:, name: 'Unknown', _parent_permission: true)
+  def initialize(age:, name: 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
+    @parent_permission = parent_permission
   end
 
   attr_accessor :name, :age
@@ -17,6 +18,6 @@ class Person
 
   # rubocop:enable Naming/PredicateName
   def can_use_services?
-    is_of_age? || parent_permission
+    is_of_age? || @parent_permission
   end
 end
