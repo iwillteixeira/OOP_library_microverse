65c05d2ae37769dac7c1894913acbe22649d53f5
iwillteixeira <iwill2005@gmail.com>
2022-03-11 14:44:02 -0300

    Add basic UI

--- main.rb
+++ main.rb
@@ -1,13 +1,45 @@
 #!/usr/bin/env ruby
-require './person'
-require './student'
-require './classroom'
-require './corrector'
-require './book'
-require './rental'
+require './app.rb'
 
-donald = Student.new(age: 18, classroom: 'none', name: 'Donald')
-p donald.play_hooky
-dracula = Book.new('robinhood', 'dracula')
-donald.add_rental(dracula, '01-02-2021')
-p donald.rentals.first.book
+def main
+  app = App.new
+  status = true
+  puts 'Welcome to School Library App!'
+  while status
+    puts
+    puts 'Please choose an option by entering a number:'
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given person id'
+    puts '7 - Exit'
+
+    option = gets.chomp
+
+    case option
+    when '1'
+      app.all_books
+    when '2'
+      app.all_people
+    when '3'
+      app.create_person
+    when '4'
+      app.create_book
+    when '5'
+      app.create_rental
+    when '6'
+      app.all_rentals_id
+    when '7'
+      puts 'Thank you for using this app!'
+      puts
+      status = false
+    else
+      puts 'Sorry, you choose a wrong option'
+      puts
+    end
+  end
+end
+
+main
